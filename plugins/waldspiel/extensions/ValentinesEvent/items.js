a2392f66e8c31ac179abb4c3bc4edf794cea3539
ble.rai <ble.rai@berufsschule.paulinenpflege.de>
2026-02-13 18:52:00 +0100

    add valentine images

--- plugins/waldspiel/extensions/ValentinesEvent/items.js
+++ plugins/waldspiel/extensions/ValentinesEvent/items.js
@@ -1,8 +1,8 @@
 const Currency = require('../../lib/Currency.js');
 
 module.exports = {
-    VALENTINE_HAT: { name: "Herzchen-Hut", filename: "../../extensions/ValentinesEvent/images/valentine_hat", price: 0, currency: Currency.BERRY },
-    VALENTINE_SCARF: { name: "Liebes-Schal", filename: "../../extensions/ValentinesEvent/images/valentine_scarf", price: 0, currency: Currency.BERRY },
-    VALENTINE_GLASSES: { name: "Rosa Brille", filename: "../../extensions/ValentinesEvent/images/valentine_glasses", price: 0, currency: Currency.BERRY },
-    VALENTINE_ROSE: { name: "Valentinstag-Rose", filename: "../../extensions/ValentinesEvent/images/valentinstagRose", price: 0, currency: Currency.BERRY }
+    VALENTINE_ROSEBUSH: { name: "Rosenstrauch", filename: "../../extensions/ValentinesEvent/images/valentine_rosebush", price: 0, currency: Currency.BERRY },
+    VALENTINE_HAIRCLIP: { name: "Haarklammer", filename: "../../extensions/ValentinesEvent/images/valentine_hairclip", price: 0, currency: Currency.BERRY },
+    VALENTINE_CHOCOLATE: { name: "Schokolade", filename: "../../extensions/ValentinesEvent/images/valentine_chocolate", price: 0, currency: Currency.BERRY },
+    VALENTINE_ROSE: { name: "Valentinstag-Rose", filename: "../../extensions/ValentinesEvent/images/valentine_rose", price: 0, currency: Currency.BERRY }
 }
